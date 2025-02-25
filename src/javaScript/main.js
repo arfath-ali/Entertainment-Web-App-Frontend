e5108837f80b405a2e431cc8e5e7cecf59d6a387
arfath-ali <1290sfaa28@gmail.com>
2025-02-25 15:49:41 +0530

    Remove navbar.html and implement navbar.js with dynamic loading logic

--- src/javaScript/main.js
+++ src/javaScript/main.js
@@ -1,1 +1,9 @@
+/* eslint-disable no-undef */
+import { loadNavbar } from './utils/navbar.js';
 
+document.addEventListener('DOMContentLoaded', () => {
+  const navbarContainer = document.querySelector('.navbar');
+  if (navbarContainer) {
+    navbarContainer.innerHTML = loadNavbar();
+  }
+});
