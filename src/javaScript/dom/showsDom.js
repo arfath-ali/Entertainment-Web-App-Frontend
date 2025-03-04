0d0f68e514213946ea7fbf2e735dae4a74327fc5
arfath-ali <1290sfaa28@gmail.com>
2025-03-04 11:21:21 +0530

    update poster sizes and styling for responsiveness and better interaction

--- src/javaScript/dom/showsDom.js
+++ src/javaScript/dom/showsDom.js
@@ -5,14 +5,17 @@ export default function showsDom() {
   const movie = document.createElement('div');
   const poster = document.createElement('img');
   const title = document.createElement('h1');
+  movie.classList.add("hover:cursor-pointer")
   poster.classList.add(
-    'min-w-[150px]',
-    'max-w-[150px]',
-    'min-h-[230px]',
-    'max-h-[230px]',
+    'min-w-[120px]',
+    'min-h-[180px]',
+    'tablet:min-w-[150px]',
+    'tablet:min-h-[225px]',
+    'desktop:min-w-[200px]',
+    'desktop:min-h-[300px]',
     'overflow-scroll',
   );
-  title.classList.add('text-15', 'text-wrap', 'text-center', 'mt-2');
+  title.classList.add('text-15', "font-weight-medium",'text-wrap', 'text-center', 'mt-2');
   movie.appendChild(poster);
   movie.appendChild(title);
   domElements.trendingShowsContainer.appendChild(movie);
