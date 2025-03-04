0a5804a43bef79f4f092ef500cadd96a77ef0d5d
arfath-ali <1290sfaa28@gmail.com>
2025-03-04 13:35:00 +0530

    add recommended shows container to domElements

--- src/javaScript/dom/domElements.js
+++ src/javaScript/dom/domElements.js
@@ -1,4 +1,5 @@
 /* eslint-disable no-undef */
 export const domElements = {
   trendingShowsContainer: document.querySelector('.trending-shows'),
+  recommendedShowsContainer: document.querySelector('.recommended-shows'),
 };
