1b1e85f60868344aeb013068e56604afdd9f2456
arfath-ali <1290sfaa28@gmail.com>
2025-03-08 14:57:14 +0530

    update api request handling in fetchShowsData

--- src/javaScript/utils/fetchShowsData.js
+++ src/javaScript/utils/fetchShowsData.js
@@ -1,57 +1,15 @@
-import fetchApiKey from './fetchApiKey.js';
-
-let apiKey;
-
 export default async function fetchShowsData(showTitle, showType) {
   try {
-    if (!apiKey) {
-      apiKey = await fetchApiKey();
-    }
+    const response = await fetch(
+      `https://entertainment-web-app-backend-p6xz.onrender.com/api/shows?title=${encodeURIComponent(showTitle)}&type=${showType}`,
+    );
 
-    const movieAPI = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${encodeURIComponent(showTitle)}`;
-    const tvAPI = `https://api.themoviedb.org/3/search/tv?api_key=${apiKey}&query=${encodeURIComponent(showTitle)}`;
+    if (!response.ok) throw new Error(`HTTP error! Status: ${response.status}`);
 
-    let response;
-    let data;
-    let foundShow;
-    let movie;
-    let tvShow;
-
-    if (showType === 'movie') {
-      response = await fetch(movieAPI);
-      if (!response.ok)
-        throw new Error(`HTTP error! Status:${response.status}`);
-
-      data = await response.json();
-      console.log(data);
-
-      foundShow =
-        data.results[0].title.toLowerCase() === showTitle.toLowerCase()
-          ? data.results[0]
-          : null;
-
-      movie = foundShow;
-      tvShow = null;
-    } else if (showType === 'webseries') {
-      response = await fetch(tvAPI);
-      if (!response.ok)
-        throw new Error(`HTTP error! Status:${response.status}`);
-
-      data = await response.json();
-      foundShow =
-        data.results[0].name.toLowerCase() === showTitle.toLowerCase()
-          ? data.results[0]
-          : null;
-
-      tvShow = foundShow;
-      movie = null;
-    } else {
-      throw new Error('Invalid show type');
-    }
-
-    return { movie, tvShow };
+    const data = await response.json();
+    return data.results[0];
   } catch (error) {
     console.log(error);
-    return { movie: null, webSerie: null };
+    return null;
   }
 }
